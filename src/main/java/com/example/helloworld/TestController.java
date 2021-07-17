c588209af66d7a3e645da045896cb4044582aacf
anilmaharjan <anilmaharjan87@gmail.com>
2021-07-17 20:01:51 +0545

    testing

--- src/main/java/com/example/helloworld/TestController.java
+++ src/main/java/com/example/helloworld/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String helloWorld() {
+        System.out.println("Testing...............................");
         return "Hello World";
     }
 }
